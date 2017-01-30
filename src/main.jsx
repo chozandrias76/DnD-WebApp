0161d003285bd49d640f7717f9353602cdd43c8f
Colin Swenson-Healey <chozandrias76@gmail.com>
2017-01-30 13:28:28 -0800

    Cleaned up code and moved styles to scss

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ const {Grid, Row, Col} = require('react-bootstrap')
 const {Link, Router, Route, browserHistory, IndexRedirect} = require('react-router')
 const CharacterComponent = require('./components/character/character.jsx');
 
-require("!style-loader!css-loader!sass-loader!./stylesheets/styles.scss")
+require("./stylesheets/styles.scss")
 
-const NoMatch = React.createClass(({
+const NoMatch = React.createClass(({//Basically our 404 page
     render: function () {
         return (
             <div>
@@ -18,26 +18,25 @@ const NoMatch = React.createClass(({
     }
 }))
 
-const MainComponent = React.createClass(({
+const MainComponent = React.createClass(({ //This is the splash page for the website
     render: function () {
         return (
             <Grid>
-                <Row className="show-grid" id="body-header">
+            {/*This top row just contains a welcome message*/}
+                <Row className="show-grid body-header">
                     <Col xs={6} md={4}></Col>
                     <Col xs={6} md={4}>
                         <h1>Welcome!</h1>
                     </Col>
                     <Col xsHidden md={4}></Col>
                 </Row>
+                {/*This row contains the two buttons for navigating away from the main page*/}
                 <Row className="show-grid" id="body-main">
                     <Col xs={6} md={6}>
+                    {/*Using Link means we pass navigation through react-router and don't actually use server side routing*/}
                         <Link
-                            className="btn btn-large btn-primary btn-block"
+                            className="btn btn-large btn-primary btn-block front-navigation-buttons"
                             role="button"
-                            style={{
-                            padding: '10px',
-                            fontSize: '20px'
-                        }}
                             to={{
                             pathname: '/characters'
                         }}>
@@ -46,12 +45,8 @@ const MainComponent = React.createClass(({
                     </Col>
                     <Col xs={6} md={6}>
                         <Link
-                            className="btn btn-large btn-primary btn-block"
+                            className="btn btn-large btn-primary btn-block front-navigation-buttons"
                             role="button"
-                            style={{
-                            padding: '10px',
-                            fontSize: '20px'
-                        }}
                             to={{
                             pathname: '/characters/new'
                         }}>
